43ba1e72d2a61352b1dfb284a0839f87f5c4ddee
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2021-05-14 10:59:25 +0100

    🔐 Update GPG key link to Keyoxide

--- src/Header.svelte
+++ src/Header.svelte
@@ -18,7 +18,7 @@
 
   <HeaderUtilities>
     <HeaderActionLink
-      href="https://keys.openpgp.org/search?q=lawrence%40lawrencethorpe.uk"
+      href="https://keyoxide.org/hkp/lawrence@lawrencethorpe.uk"
       target="_blank"
       icon={{ render: FingerprintRecognition20 }} />
     <HeaderActionLink
