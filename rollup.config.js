46d15e34a3ba25dadc7fba70443b8306db548f26
Lawrence Thorpe <lawrence@lawrencethorpe.uk>
2025-04-20 11:49:18 +0100

    update footer

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
+import svg from "rollup-plugin-svg";
 import { babel } from "@rollup/plugin-babel";
 
 const production = !process.env.ROLLUP_WATCH;
@@ -54,6 +55,8 @@ export default {
     // we'll extract any component CSS out into
     // a separate file - better for performance
     css({ output: "bundle.css" }),
+
+    svg(),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-  import { Link } from "carbon-components-svelte";
+  import { Link, Tooltip } from "carbon-components-svelte";
   import LogoGithub from "carbon-icons-svelte/lib/LogoGithub32";
   import LogoLinkedin from "carbon-icons-svelte/lib/LogoLinkedin32";
-  import LogoTwitter from "carbon-icons-svelte/lib/LogoTwitter32";
   import LogoInstagram from "carbon-icons-svelte/lib/LogoInstagram32";
-  import LogoSnapchat from "carbon-icons-svelte/lib/LogoSnapchat32";
-  import LogoFacebook from "carbon-icons-svelte/lib/LogoFacebook32";
   import LogoYoutube from "carbon-icons-svelte/lib/LogoYoutube32";
   import Email from "carbon-icons-svelte/lib/Email32";
+  import BGG from "./icons/bgg.svg";
 </script>
 
 <style lang="scss">
@@ -23,25 +21,19 @@
   <Link href="https://github.com/thorpelawrence">
     <LogoGithub />
   </Link>
-  <Link href="https://www.linkedin.com/in/thorpelawrence/">
+  <Link href="https://linkedin.com/in/thorpelawrence">
     <LogoLinkedin />
-  </Link>
-  <Link href="https://twitter.com/thorpe_lawrence">
-    <LogoTwitter />
   </Link>
   <Link href="https://instagram.com/thorpe_lawrence">
     <LogoInstagram />
   </Link>
-  <Link href="https://www.snapchat.com/add/thorpe_lawrence">
-    <LogoSnapchat />
+  <Link href="https://boardgamegeek.com/user/thorpe_lawrence" class="footer-icon">
+    {@html BGG}
   </Link>
-  <Link href="https://www.facebook.com/thorpe.lawrence">
-    <LogoFacebook />
-  </Link>
-  <Link href="https://www.youtube.com/c/lawrencethorpe">
+  <Link href="https://youtube.com/c/lawrencethorpe">
     <LogoYoutube />
   </Link>
-  <Link href="mailto:lawrence@lawrencethorpe.uk">
-    <Email />
-  </Link>
+  <Tooltip direction="top" icon={Email}>
+    lawrence at lawrencethorpe dot uk
+  </Tooltip>
 </footer>
